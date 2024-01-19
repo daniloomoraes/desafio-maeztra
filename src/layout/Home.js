af401dd29811e79e36abd91c9c99c56d89771f7b
Danilo Moraes <daniloomoraes@gmail.com>
2024-01-19 18:45:29 -0300

    feat: info sobre

--- src/layout/Home.js
+++ src/layout/Home.js
@@ -1,6 +1,9 @@
-import Slider from '../components/Home/Slider';
-import TipBar from '../components/Home/TipBar';
-import Marcas from '../components/Home/Marcas';
+import Slider from "../components/Home/Slider";
+import TipBar from "../components/Home/TipBar";
+import Marcas from "../components/Home/Marcas";
+
+import bannerSobre from "../assets/img/home/banner-sobre.png";
+import bannerSobreMobile from "../assets/img/home/banner-sobre-mobile.png";
 
 function Home() {
   return (
@@ -8,6 +11,28 @@ function Home() {
       <Slider />
       <TipBar />
       <Marcas />
+      <div className="bannerSobreContainer">
+        <div className="bannerSobreRow">
+          <div className="bannerSobreText">
+            <h2>Lorem ipsum </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum
+              mauris posuere fusce proin mattis. Hendrerit cras ut nunc enim
+              dictum. Mattis proin ut quis donec sed eget nulla. Vel mi ut
+              mauris integer. Nibh sagittis in lobortis sed cursus condimentum
+              velit pharetra. Amet luctus ut vulputate scelerisque placerat
+              consequat. Neque arcu mi iaculis id. Vel vitae, pharetra, a nec
+              tristique. Feugiat id tortor eu mauris pulvinar velit massa. Ut
+              ornare augue eget convallis volutpat aliquet. Sed sed pellentesque
+              porttitor phasellus donec condimentum sit sapien.
+            </p>
+          </div>
+          <div className="bannerSobreImagens">
+            <img src={bannerSobreMobile} className="bannerSobreImagemMobile" alt="Banner Sobre" />
+            <img src={bannerSobre} className="bannerSobreImagemDesktop" alt="Banner Sobre" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
