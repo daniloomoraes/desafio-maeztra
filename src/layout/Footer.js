7a8d61d979a9e84073ca93b15a2b3ee7d6b52341
Danilo Moraes <daniloomoraes@gmail.com>
2024-01-19 14:34:31 -0300

    feat: footer sobre

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,10 +1,19 @@
 import Newsletter from "../components/Footer/Newsletter";
 
+import icoFacebook from "../assets/img/footer/ico-facebook.svg";
+import icoLinkedin from "../assets/img/footer/ico-linkedin.svg";
+import icoInstagram from "../assets/img/footer/ico-instagram.svg";
+import icoYoutube from "../assets/img/footer/ico-youtube.svg";
+import icoVisa from "../assets/img/footer/ico-visa.svg";
+import icoMastercard from "../assets/img/footer/ico-mastercard.svg";
+import logoMaeztraFooter from "../assets/img/footer/logo-maeztra.png";
+import logoVtex from "../assets/img/footer/logo-vtex.png";
+
 function Footer() {
   return (
     <>
       <Newsletter />
-      <div className="footerMenuContainer">
+      <footer className="footerMenuContainer">
         <div className="footerMenuRow">
           <div className="footerMenuCol">
             <a href="/" className="footerMenuTitle">
@@ -53,6 +62,44 @@ function Footer() {
                 <a href="/">Endereço</a>
               </li>
             </ul>
+          </div>
+        </div>
+      </footer>
+      <div className="footerSobreContainer">
+        <div className="footerSobreRow">
+          <div className="footerSobreCol">
+            <a href="/">
+              <img src={icoFacebook} alt="Ícone Facebook" />
+            </a>
+            <a href="/">
+              <img src={icoLinkedin} alt="Ícone Linkedin" />
+            </a>
+            <a href="/">
+              <img src={icoInstagram} alt="Ícone Instagram" />
+            </a>
+            <a href="/">
+              <img src={icoYoutube} alt="Ícone Youtube" />
+            </a>
+          </div>
+          <div className="footerSobreCol">
+            <img src={icoVisa} alt="Ícone Visa" />
+            <img src={icoMastercard} alt="Ícone MasterCard" />
+            <img src={icoVisa} alt="Ícone Visa" />
+            <img src={icoMastercard} alt="Ícone MasterCard" />
+          </div>
+          <div className="footerSobreCol">
+            <div className="footerSobreMarcas">
+              <p>Powered by</p>
+              <a href="/">
+                <img src={logoVtex} alt="Logo Vtex" />
+              </a>
+            </div>
+            <div className="footerSobreMarcas">
+              <p>Developed by</p>
+              <a href="/">
+                <img src={logoMaeztraFooter} alt="Logo Maeztra" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
