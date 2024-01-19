46a207c1d89e684a5df9cb271f855ef23a29345e
Danilo Moraes <daniloomoraes@gmail.com>
2024-01-19 14:04:32 -0300

    feat: menu footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -4,8 +4,57 @@ function Footer() {
   return (
     <>
       <Newsletter />
-      <div>
-        Footer
+      <div className="footerMenuContainer">
+        <div className="footerMenuRow">
+          <div className="footerMenuCol">
+            <a href="/" className="footerMenuTitle">
+              Informações
+            </a>
+            <ul>
+              <li>
+                <a href="/">Quem Somos</a>
+              </li>
+              <li>
+                <a href="/">Prazo de Envio</a>
+              </li>
+              <li>
+                <a href="/">Trocas e Devoluções</a>
+              </li>
+              <li>
+                <a href="/">Promoções e Cupons</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footerMenuCol">
+            <a href="/" className="footerMenuTitle">
+              Minha Conta
+            </a>
+            <ul>
+              <li>
+                <a href="/">Minha Conta</a>
+              </li>
+              <li>
+                <a href="/">Meus Pedidos</a>
+              </li>
+              <li>
+                <a href="/">Cadastre-se</a>
+              </li>
+            </ul>
+          </div>
+          <div className="footerMenuCol">
+            <a href="/" className="footerMenuTitle">
+              Onde nos Encontrar
+            </a>
+            <ul>
+              <li>
+                <a href="/">Lojas</a>
+              </li>
+              <li>
+                <a href="/">Endereço</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
